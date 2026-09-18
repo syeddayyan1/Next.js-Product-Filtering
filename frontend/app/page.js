5cff850fdcb5196ebc7582b68995691f13fbf809
syeddayyan1 <sddayyan36@gmail.com>
2026-09-18 11:33:17 +0500

    Project Updated

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -164,7 +164,7 @@ const filteredProducts = products.filter((product) => {
                 
                 <button onClick={() => router.push(`/products/${product.id}`)}
                   className="rounded-lg bg-blue-600 px-4 py-2 text-sm font-semibold
-             text-white transition hover:bg-blue-700">
+             text-white transition hover:bg-blue-700 mt-3 mr-1">
                 
                View Details
 
